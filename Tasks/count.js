d29496add3ae77b03defa017816e48525aed15a2
Eduard Kyrychenko <eduardkyrychenko923@Alexs-MacBook-Pro.local>
2025-01-14 16:29:47 +0200

    step 2: remove empty blocks

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,18 +1,17 @@
 // Refactor following solution
 // Sum all number values in dict
+
+// step 2: remove empty blocks
+
 'use strict';
 
 count = (obj) => {
   sum = 0;
-  {
-  }
   keys = Object.keys(obj);
   keys.forEach((key) => {
-    [];
     const value = obj[key];
     if (typeof value === 'number') sum += value;
   });
-  ({});
   return sum;
 };
 
