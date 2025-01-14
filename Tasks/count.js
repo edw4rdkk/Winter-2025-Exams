d4a40c8df7c3fbdcfa85bfdd8b31eefc591083a6
Eduard Kyrychenko <eduardkyrychenko923@Alexs-MacBook-Pro.local>
2025-01-14 16:39:56 +0200

    step 4: replace forEach with for of loop

--- Tasks/count.js
+++ Tasks/count.js
@@ -1,17 +1,17 @@
 // Refactor following solution
 // Sum all number values in dict
 
-// step 3: add let and const
+// step 4: replace forEach wit for of loop
 
 'use strict';
 
 const count = (obj) => {
   let sum = 0;
   const keys = Object.keys(obj);
-  keys.forEach((key) => {
+  for (const key of keys) {
     const value = obj[key];
     if (typeof value === 'number') sum += value;
-  });
+ }
   return sum;
 };
 
