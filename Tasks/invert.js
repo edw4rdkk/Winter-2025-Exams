7a52bce3751011537fbfc756712088921c05aef9
Eduard Kyrychenko <eduardkyrychenko923@Alexs-MacBook-Pro.local>
2025-01-15 11:23:46 +0200

    step 1: add 'use strict'

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,6 +1,10 @@
 // Refactor following solution
 // Reverse an array, you can't use .reverse()
 
+// step 1: add 'use strict'
+
+'use strict';
+
 invert = (A, i, j, k) => {
   T = Object.keys(A, 4);
   T.forEach((_, i) => {
