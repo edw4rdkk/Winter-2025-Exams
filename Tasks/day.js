fdad7a24a215bb838b0f64992058dd0c9c8237a4
Eduard Kyrychenko <eduardkyrychenko923@Alexs-MacBook-Pro.local>
2025-01-15 10:06:58 +0200

    step 2: add const, fix naming, initialize i directly in the loop

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,20 +1,19 @@
 // Refactor following solution
 // Get day number
 
-// step 1: add 'use strict'
+// step 2: add const, fix naming, initialize i directly in the loop
 
 'use strict';
 
-let D = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-_parse_day_ = (s) => {
-  let i;
-  for (i = 0; i < D.length; i++) {
-    if (s.startsWith(D[i].toLowerCase())) {
+const getDayNumber = (input) => {
+  for (let i = 0; i < DAYS.length; i++) {
+    if (input.startsWith(DAYS[i].toLowerCase())) {
       return i + 1;
     }
   }
   return -1;
 };
 
-module.exports = _parse_day_;
+module.exports = getDayNumber;
