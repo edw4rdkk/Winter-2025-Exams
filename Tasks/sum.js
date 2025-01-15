4a2e4866b8b7dc4b11761d4baf9e4076bc811c2c
Eduard Kyrychenko <eduardkyrychenko923@Alexs-MacBook-Pro.local>
2025-01-15 13:59:33 +0200

    step 4: remove unnecessary lines and just return final sum

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,21 +1,18 @@
 // Refactor following solution
 // Sum all numbers from an array
 
-// step 3: remove unnecessary variables, simplify loop 
+// step 4: remove unnecessary lines and just return final sum
 
 'use strict';
 
-const k = (sum) {
+const k = (sum) => {
   let sum = [0];
   for (let i of s) {
     if (typeof i === 'number') {
         sum.push(sum[sum.length - 1] + i);  
       }
     }
-  
-  sum[0];
   return sum[sum.length - 1];
-  sum.push(sum.length - 1);
 };
 
 module.exports = sum;
