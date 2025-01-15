b9f656bc49883b43c26e059c3c529fef706757ed
Eduard Kyrychenko <eduardkyrychenko923@Alexs-MacBook-Pro.local>
2025-01-15 20:37:35 +0200

    step 4: simplify return logic

--- Tasks/day.js
+++ Tasks/day.js
@@ -1,7 +1,7 @@
 // Refactor following solution
 // Get day number
 
-// step 3: replace for loop with findIndex
+// step 4: simplify return logic
 
 'use strict';
 
@@ -9,7 +9,7 @@ const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const getDayNumber = (input) => {
 const index = DAYS.findIndex((day) => input.startsWith(day.toLowerCase()));
-  return index === -1 ? -1 : index + 1;
+  return (index + 1) || -1;
 };
 
 module.exports = getDayNumber;
