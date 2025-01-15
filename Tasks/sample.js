4fe5526965b59726d9af60835ce54553d74e9301
Eduard Kyrychenko <eduardkyrychenko923@Alexs-MacBook-Pro.local>
2025-01-15 12:03:57 +0200

    step 3: directly return random element

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,13 +1,10 @@
 // Refactor following solution
 // Get one random element from an array
 
-// step 2: avoid mutating function input parameter
+// step 3: directly return random element
 
 'use strict';
 
-const SAMPLE = (arr) => {
-  const randomElement = arr[Math.floor(Math.random() * arr.length)];
-  return randomElement;
-};
+const SAMPLE = (arr) =>  arr[Math.floor(Math.random() * arr.length)];
 
 module.exports = SAMPLE;
