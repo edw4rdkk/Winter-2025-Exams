aa251e6b111885369189584686f49594842d7404
Eduard Kyrychenko <eduardkyrychenko923@Alexs-MacBook-Pro.local>
2025-01-15 12:05:39 +0200

    step 4: fix naming for better understanding

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,10 +1,10 @@
 // Refactor following solution
 // Get one random element from an array
 
-// step 3: directly return random element
+// step 4: fix naming for better understanding
 
 'use strict';
 
-const SAMPLE = (arr) =>  arr[Math.floor(Math.random() * arr.length)];
+const getRandomElement = (arr) =>  arr[Math.floor(Math.random() * arr.length)];
 
-module.exports = SAMPLE;
+module.exports = getRandomElement;
