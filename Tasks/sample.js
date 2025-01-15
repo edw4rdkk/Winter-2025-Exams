80fbff8e33305a7d8c26c97e5e00e9d5f9a59ca0
Eduard Kyrychenko <eduardkyrychenko923@Alexs-MacBook-Pro.local>
2025-01-15 11:56:06 +0200

    step 1: add 'use strcit'

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,6 +1,10 @@
 // Refactor following solution
 // Get one random element from an array
 
+// step 1: add 'use strcit'
+
+'use strict';
+
 const SAMPLE = (arr) => {
   arr = arr[Math.floor(Math.random() * arr.length)];
   return arr;
