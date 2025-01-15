2c17501ba2d09b6753da731f5f71a6c2421a766f
Eduard Kyrychenko <eduardkyrychenko923@Alexs-MacBook-Pro.local>
2025-01-15 14:22:52 +0200

    step 1: add 'use strict'

--- Tasks/password.js
+++ Tasks/password.js
@@ -1,6 +1,10 @@
 // Refactor following solution
 // Generate random password
 
+// step 1: add 'use strict'
+
+'use strict';
+
 let GeneratePassword = (alphabet, length) => {
   const MAX = alphabet.length;
   key = '';
